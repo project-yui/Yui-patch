4656ea4b8875ef05e1eb59054da70aa40522b3af
msojocs <jiyecafe@gmail.com>
2024-04-29 21:43:53 +0800

    fix: ci

---
+++ tools/windows/ci.ps1
@@ -0,0 +1,13 @@
+param($arch, $tag)
+$root_dir = Resolve-Path (Join-Path $PSScriptRoot "../../")
+
+cd $root_dir
+try
+{
+    cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$root_dir" -B"$root_dir/build" -G Ninja
+    cmake --build "$root_dir/build" --config Debug --target nt_native --
+    mkdir "$root_dir/tmp/build"
+    mv "$root_dir/build/dbghelp.dll" "$root_dir/tmp/build/yukihana-dbghelp-win32-$arch-$tag.dll"
+}catch{
+    exit 1
+}
