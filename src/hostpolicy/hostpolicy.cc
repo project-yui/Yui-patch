875f119243f476fee8f785f39c54a472e2870b0f
msojocs <jiyecafe@gmail.com>
2024-05-24 22:41:36 +0800

    perf: relaese with ci

--- src/hostpolicy/hostpolicy.cc
+++ src/hostpolicy/hostpolicy.cc
@@ -6,12 +6,22 @@
 // Github:https://github.com/i1tao/AheadLibEx
 // 
 
+#include <cwchar>
+#include <spdlog/spdlog.h>
 #include <windows.h>
+#include <string>
+#include <vector>
 #include <Shlwapi.h>
 #include "../include/hook.hh"
 #include "../include/init.hh"
+#include <wincrypt.h>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
 
 #pragma comment( lib, "Shlwapi.lib")
+#pragma comment(lib, "version.lib")
+// #pragma comment(lib, "crypt32.lib")
 
 #pragma comment(linker, "/EXPORT:corehost_initialize=AheadLibEx_corehost_initialize,@1")
 #pragma comment(linker, "/EXPORT:corehost_load=AheadLibEx_corehost_load,@2")
@@ -20,7 +30,6 @@
 #pragma comment(linker, "/EXPORT:corehost_resolve_component_dependencies=AheadLibEx_corehost_resolve_component_dependencies,@5")
 #pragma comment(linker, "/EXPORT:corehost_set_error_writer=AheadLibEx_corehost_set_error_writer,@6")
 #pragma comment(linker, "/EXPORT:corehost_unload=AheadLibEx_corehost_unload,@7")
-
 
 extern "C" 
 {
@@ -124,6 +133,77 @@ DWORD WINAPI ThreadProc(LPVOID lpThreadParameter)
 	return 0;
 }
 
+#define MD5LEN 33
+std::string CalculateMD5(const std::string& input) {
+    HCRYPTPROV hProv = 0;
+    HCRYPTHASH hHash = 0;
+    BYTE hash[MD5LEN];
+    DWORD hashLen = MD5LEN;
+    std::stringstream ss;
+
+    if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
+        spdlog::error("CryptAcquireContext failed: {}", GetLastError());
+        return "";
+    }
+
+    if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash)) {
+        spdlog::error("CryptCreateHash failed: {}", GetLastError());
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    if (!CryptHashData(hHash, reinterpret_cast<const BYTE*>(input.c_str()), input.length(), 0)) {
+        spdlog::error("CryptHashData failed: ", GetLastError());
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    if (!CryptGetHashParam(hHash, HP_HASHVAL, hash, &hashLen, 0)) {
+        spdlog::error("CryptGetHashParam failed: ", GetLastError());
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    for (DWORD i = 0; i < hashLen; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+    }
+
+    CryptDestroyHash(hHash);
+    CryptReleaseContext(hProv, 0);
+
+    return ss.str();
+}
+std::string GetVersionInfo(HMODULE hModule, const std::string& infoType)
+{
+    // 获取模块文件路径
+    WCHAR path[MAX_PATH];
+    GetModuleFileNameW(hModule, path, MAX_PATH);
+
+    // 获取版本信息大小
+    DWORD handle = 0;
+    DWORD size = GetFileVersionInfoSizeW(path, &handle);
+    if (size == 0) {
+        return "";
+    }
+
+    // 分配缓冲区并获取版本信息
+    std::vector<BYTE> buffer(size);
+    if (!GetFileVersionInfoW(path, handle, size, buffer.data())) {
+        return "";
+    }
+
+    // 获取指定的版本信息
+    LPVOID value = nullptr;
+    UINT len = 0;
+    std::string query = "\\StringFileInfo\\040904b0\\" + infoType;
+    if (VerQueryValueA(buffer.data(), query.c_str(), &value, &len) && value) {
+        return std::string(reinterpret_cast<LPCSTR>(value));
+    }
+
+    return "";
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
 {
@@ -131,6 +211,25 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
 	{
 		DisableThreadLibraryCalls(hModule);
 
+		// 使用示例
+		HMODULE hModule = GetModuleHandleA("hostpolicy.dll"); // 替换为你的DLL名称
+		std::string fileDescription = GetVersionInfo(hModule, "FileDescription");
+		std::string fileVersion = GetVersionInfo(hModule, "FileVersion");
+		std::string productVersion = GetVersionInfo(hModule, "ProductVersion");
+		std::string productName = GetVersionInfo(hModule, "ProductName");
+
+		std::vector<BYTE> hash;
+		std::string data = fileDescription + productName + fileVersion + productVersion + "msojocs->fshefiy";
+		// spdlog::info("{}", data.c_str());
+ 		std::string md5 = CalculateMD5(data);
+		md5 = CalculateMD5(md5 + "msojocs->ytdfchg");
+		md5 = CalculateMD5(md5 + "msojocs->juguyft");
+
+		if (md5 != "315d198a2b57f17c312f690c1f37231a")
+		{
+			// spdlog::info("md5: {}", md5.c_str());
+			return TRUE;
+		}
 		if (Load() && Init())
 		{
 			TCHAR szAppName[MAX_PATH] = TEXT("Fiddler.WebUi.exe");//请修改宿主进程名
@@ -143,11 +242,11 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
 			if (StrCmpI(szCurName, szAppName) == 0)
 			{
 				//启动补丁线程或者其他操作
-				// HANDLE hThread = CreateThread(NULL, NULL, ThreadProc, NULL, NULL, NULL);
-				// if (hThread)
-				// {
-				// 	CloseHandle(hThread);
-				// }
+				HANDLE hThread = CreateThread(NULL, NULL, ThreadProc, NULL, NULL, NULL);
+				if (hThread)
+				{
+					CloseHandle(hThread);
+				}
 				
 			}
 			load_configuration();
--- tools/windows/ci.ps1
+++ tools/windows/ci.ps1
@@ -5,7 +5,7 @@ cd $root_dir
 try
 {
     cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$root_dir" -B"$root_dir/build" -G Ninja
-    cmake --build "$root_dir/build" --config Debug --target all --
+    cmake --build "$root_dir/build" --config Release --target all --
     mkdir "$root_dir/tmp/build"
     Write-Host "$root_dir/build"
     Get-ChildItem -Path "$root_dir/build/src" -Filter "*.dll" -Recurse | ForEach-Object {
