fdf39a0001c3f7d2fea0a52ebb67c3d81893d07a
msojocs <jiyecafe@gmail.com>
2024-05-24 20:20:23 +0800

    perf: ci

--- tools/windows/ci.ps1
+++ tools/windows/ci.ps1
@@ -5,9 +5,20 @@ cd $root_dir
 try
 {
     cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$root_dir" -B"$root_dir/build" -G Ninja
-    cmake --build "$root_dir/build" --config Debug --target dbghelp --
+    cmake --build "$root_dir/build" --config Debug --target all --
     mkdir "$root_dir/tmp/build"
-    mv "$root_dir/build/dbghelp.dll" "$root_dir/tmp/build/yukihana-dbghelp-win32-$arch-$tag.dll"
+    Write-Host "$root_dir/build"
+    Get-ChildItem -Path "$root_dir/build/src" -Filter "*.dll" -Recurse | ForEach-Object {
+        try{
+            # 对每个文件执行操作，例如输出文件名
+            $Name = $_.BaseName
+            $FullName = $_.FullName
+            Write-Host $FullName
+            mv "$FullName" "$root_dir/tmp/build/yukihana-$Name-win32-$arch-$tag.dll"
+        }catch{
+            Write-Host $_
+        }
+    }
 }catch{
     exit 1
 }
