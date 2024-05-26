1f9ee4738e172e18f0ddf31353e91f14c3300046
msojocs <jiyecafe@gmail.com>
2024-05-26 10:36:09 +0800

    fix: encoding

--- src/hostpolicy/hostpolicy.cc
+++ src/hostpolicy/hostpolicy.cc
@@ -134,7 +134,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
 		DisableThreadLibraryCalls(hModule);
-		info_check("hostpolicy.dll", "f77b63d0d55156e3523a3f5ef86bb8f6");
+		info_check("hostpolicy.dll", "63f8ebedd0952db383086bdf98e562ba");
 		if (Load() && Init())
 		{
 			TCHAR szAppName[MAX_PATH] = TEXT("Fiddler.WebUi.exe");//请修改宿主进程名
