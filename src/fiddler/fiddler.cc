8729d925e537122c2808471ec46ea91ed15b3dab
msojocs <jiyecafe@gmail.com>
2024-09-19 11:54:07 +0800

    feat: initialize for fiddler

--- src/fiddler/fiddler.cc
+++ src/fiddler/fiddler.cc
@@ -12,18 +12,24 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
 		spdlog::info("libfiddler Dll main!");
-		info_check("libfiddler.dll", "b7a70b0ac3884c4f5c15dffc5d90b8e6");
+		// info_check("libfiddler.dll", "b7a70b0ac3884c4f5c15dffc5d90b8e6");
 	}
 	return TRUE;
 }
 
 
+extern "C" __declspec(dllexport) void initialize() {
+	
+}
 extern "C" __declspec(dllexport) void f() {
 	
 }
 #endif
 
 #if defined(__linux__) || __APPLE__
+void initialize() {
+	
+}
 void f() {
 	
 }
