1b07d58604fa27b739d48f3ba6e75637dcf96e3c
msojocs <jiyecafe@gmail.com>
2026-05-03 20:21:25 +0800

    feat: 自动重启

--- src/fiddler/fiddler.cc
+++ src/fiddler/fiddler.cc
@@ -3,10 +3,16 @@
 #include <spdlog/spdlog.h>
 #include <fstream>
 #include <string>
+#include <vector>
 #ifdef WIN32
 #include <windows.h>
 #include <winnt.h>
 #include "../include/info_check.hh"
+#endif
+#ifdef __linux__
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
 #endif
 
 bool fixPackageJson() {
@@ -40,6 +46,61 @@ bool fixPackageJson() {
 	}
 	return false;
 }
+
+#ifdef WIN32
+bool restartCurrentProcess() {
+	std::string cmd = GetCommandLineA();
+	STARTUPINFOA si{};
+	si.cb = sizeof(si);
+	PROCESS_INFORMATION pi{};
+
+	// CreateProcess may modify command line buffer.
+	std::vector<char> cmdBuffer(cmd.begin(), cmd.end());
+	cmdBuffer.push_back('\0');
+
+	BOOL ok = CreateProcessA(
+		NULL,
+		cmdBuffer.data(),
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi
+	);
+
+	if (ok) {
+		CloseHandle(pi.hThread);
+		CloseHandle(pi.hProcess);
+		return true;
+	}
+	return false;
+}
+#endif
+
+#ifdef __linux__
+bool restartCurrentProcess() {
+	char exePath[PATH_MAX] = {0};
+	ssize_t len = readlink("/proc/self/exe", exePath, sizeof(exePath) - 1);
+	if (len <= 0) {
+		return false;
+	}
+	exePath[len] = '\0';
+
+	pid_t pid = fork();
+	if (pid < 0) {
+		return false;
+	}
+	if (pid == 0) {
+		execl(exePath, exePath, static_cast<char*>(NULL));
+		_exit(127);
+	}
+	return true;
+}
+#endif
+
 #ifdef WIN32
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
@@ -55,13 +116,17 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 extern "C" __declspec(dllexport) void initialize() {
 	if (fixPackageJson()) {
 		MessageBoxA(NULL, "package.json was fixed, please restart", "libfiddler", MB_OK | MB_ICONINFORMATION);
-		exit(1);
+		if (restartCurrentProcess()) {
+			ExitProcess(0);
+		}
 	}
 }
 extern "C" __declspec(dllexport) void f() {
 	if (fixPackageJson()) {
 		MessageBoxA(NULL, "package.json was fixed, please restart", "libfiddler", MB_OK | MB_ICONINFORMATION);
-		exit(1);
+		if (restartCurrentProcess()) {
+			ExitProcess(0);
+		}
 	}
 }
 #endif
@@ -72,12 +137,22 @@ void initialize() {
 	spdlog::info("libfiddler initialize");
 	if (fixPackageJson()) {
 		spdlog::info("package.json was fixed, please restart");
+#ifdef __linux__
+		if (restartCurrentProcess()) {
+			std::exit(0);
+		}
+#endif
 	}
 }
 void f() {
 	spdlog::info("libfiddler f");
 	if (fixPackageJson()) {
 		spdlog::info("package.json was fixed, please restart");
+#ifdef __linux__
+		if (restartCurrentProcess()) {
+			std::exit(0);
+		}
+#endif
 	}
 }
 }
